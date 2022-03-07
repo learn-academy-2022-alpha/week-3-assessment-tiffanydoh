6817ca73a909f9100c78d3938708ec6ad6cee94d
tiffanydoh <tiffanydoh@gmail.com>
2022-03-06 22:45:57 -0800

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+const { expect } = require("@jest/globals")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,12 +16,46 @@
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
+    //Psuedo-Code for Jest Test:
+        // A describe method that lists the name of the function OR naming of the particular test.
+        describe("fibArr", () => {
+            // A test/it method, nested within the describe block, that in plain words, describe what the function does. 
+            it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+        // Example input: 6
+        // Expected output: [1, 1, 2, 3, 5, 8]
+         
+        // Example input: 10
+        // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55] 
+
+               // An expect method, nested within the test block, calling on findOdds function, followed by the toEqual() matcher that checks the expected output of the function return. Create 2 expects because there are two arrays to be passed as arguments. 
+               expect(fibArr(6)).toEqual( [1, 1, 2, 3, 5, 8] )
+               expect(fibArr(10)).toEqual( [1, 1, 2, 3, 5, 8, 13, 21, 34, 55] )
+            })
+         })
 
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
-
+         
 // Example input: 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]         
+
+// b) Create the function that makes the test pass.
+         //Psuedo-Code:
+            //Input: A number
+            //Output: An array of the fibbonacci sequence with the length of what the input number is.
+            //Process:
+                // Create a function called fibArr that takes in number as a parameter.
+                //To get the first part of the fibbonacci sequence [1,1], it needs to be declared as a variable because the way the fibonacci seq works is that the 2 previous numbers sums up to make the next number. But since the seq starts with 1,1, it needs to be hardcoded. 
+                // Create a for loop where it starts at 2, keeps running until it reaches the number input.
+                //Then use the built in method .push to pysh more values in the array.
+                // Use fib[i-2] + fib[i-1] to get the next values. 
+const fibArr = (n) => {
+    let fib = [1, 1]
+    for(let i = 2; i < n ; i++) {
+        fib.push(fib[i-2] + fib[i-1] )
+    }
+    return fib
+}
 
 
 // b) Create the function that makes the test pass.
@@ -28,29 +64,81 @@
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
-
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [-823, 7, 23]
+    //Psuedo-Code for Jest Test:
+        // A describe method that lists the name of the function OR naming of the particular test.
+        describe("findOdds", () => {
+           // A test/it method, nested within the describe block, that in plain words, describe what the function does. 
+           it("returns an array of only the odd numbers, which is then sorted from least to greatest", () => {
+            const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+            // Expected output: [-9, 7, 9, 199]
+            
+            const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+            // Expected output: [-823, 7, 23]
+              // An expect method, nested within the test block, calling on findOdds function, followed by the toEqual() matcher that checks the expected output of the function return. Create 2 expects because there are two arrays to be passed as arguments. 
+              expect(findOdds(fullArr1)).toEqual( [-9, 7, 9, 199] )
+              expect(findOdds(fullArr2)).toEqual( [-823, 7, 23] )
+           })
+        })
+        
+        
 
 
 // b) Create the function that makes the test pass.
+        //Pseudo-Code:
+            // Input: fullArr1 and fullArr2
+            // Output: Only the odd numbers from least to greatest. fullArr1 = Expected output: [-9, 7, 9, 199] and fullArr2 = Expected output: [-823, 7, 23]
+            // Process:
+                // Create a function called findOdds that takes array as the parameter. 
+                // Declare a variable called oddOnly, take in that array and use .filter to loop through an array. 
+                // Using typeof, only return the value that is not a string, boolean, null. And only return the value that is odd. 
+                // Return the array sorted from least to greatest using the buit in method .sort().
+const findOdds = (array) => {
+let oddOnly =  array.filter(value => {
+        return typeof value !== "string" && typeof value !== "boolean" && typeof value !== null && value % 2 !== 0 
+    })
+    return oddOnly.sort((a,b) => a - b)
+    
+}
+
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
-
-const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
-
-const numbersToAdd3 = []
-// Expected output: []
+//Psuedo-Code for Jest Test:
+        // A describe method that lists the name of the function OR naming of the particular test.
+        describe("accSum", () => {
+            // A test/it method, nested within the describe block, that in plain words, describe what the function does. 
+            it("takes in an array and returns an array of the accumulating sum. If it is an empty array, it should return an empty array back.", () => {
+                const numbersToAdd1 = [2, 4, 45, 9]
+                // Excpected output: [2, 6, 51, 60]
+                
+                const numbersToAdd2 = [0, 7, -8, 12]
+                // Expected output: [0, 7, -1, 11]
+                
+                const numbersToAdd3 = []
+                // Expected output: []
+            
+               // An expect method, nested within the test block, calling on findOdds function, followed by the toEqual() matcher that checks the expected output of the function return. Create 2 expects because there are two arrays to be passed as arguments. 
+               expect(accSum(numbersToAdd1)).toEqual( [2, 6, 51, 60] )
+               expect(accSum(numbersToAdd2)).toEqual( [0, 7, -1, 11] )
+               expect(accSum(numbersToAdd3)).toEqual( [] )
+            })
+         })
 
 
 // b) Create the function that makes the test pass.
+         //Psuedo-Code: 
+            // Create a function called accSum that takes in an array as a parameter. 
+            // Start by declaring a variable num equal to zero because at first instance no number is accumlating.
+            // Using the built in method .map(),it will iterate through each value in the array. 
+            // Reassign num each time so that value + num is the next value in the array. It is constantly reassigning num to new value. 
+            // Return sum1 
+const accSum = (array) => {
+   let num = 0
+    let sum1 = array.map(value => {
+     num = value + num
+    return num
+    })
+    return sum1
+}
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,58 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: The keyword "this" refers to the object that is currently or specifically being invoked in a function.
 
-  Researched answer:
+  Researched answer: When calling a function as a method of the object, using "this" refers to that specific object being used or being referred to. 
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is a Javascript library for building user interfaces. React contains code snippet that is reusable. It is component based. 
 
-  Researched answer:
+  Researched answer: React is a front-end Javascript Library that is used for building user interfaces. It can be used to create web applications and mobile apps. Because React is a library, it has reusuable code snippers and other UI components that allows it to be usuable. 
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: render() is required in a React class component. By using render(), it presents the specified HTML on the webpage. 
 
-  Researched answer:
-
+  Researched answer: In React, render() method is the only required method in a class component. It describes how the webpage should display the HTML element and tags. We are able to read React props and states.
 
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX is a hybrid between HTML and Javascript.  JSX fragments ( <> </>) can be used to turn HTML tags into react elements. The difference between HTML and JSX is that HTML is a markup language that allows for creating a structure of a page. But with JSX, it is like an extension where HTML can be used within Javascript code. 
 
-  Researched answer:
+  Researched answer: JSX is Javascript XML. JSX allows us to work with HTML in React within a Javascript code. It is easier for HTML to be changed or used. HTML is the standard langauge that makes the structure of a page. JSX on the other hand, allows developers to write HTML with their javascript code in React. 
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is a package manager. It organizes node modules. 
 
-  Researched answer:
+  Researched answer: Once yarn runs, it is going to unpack all the stored dependencies that will aloow HTML and Javascript to interact together. When yarn is run, the files that get added are the node modules, package.json, yarn.lock, and .gitignore.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: I believe an anonymous function in Javascript is an unknown function.
 
-  Researched answer:
+  Researched answer: Anoymous Function is a function that does not have a name affiliated with it. 
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Works similar to how Javascript conditions work. In React, conditional rendering renders different components based on a condition. You can create many different components with different behaviors, and it will only render some of it depending on what your conditions are. 
 
-2. Props:
+2. Props: Props is used to pass information (data and methods) from the parent component to the child component. They are considered "Read Only" because they cannot be changed/updated. 
 
-3. JavaScript Events:
+3. JavaScript Events: Javascript Events is Javascript coded resposnes that interact with an HTML event. HTML events occur when either the user or browser manipulates a page. For example, we have worked with onClick which is a common event type. We can have Javascript code interact with this event by having something happen when something is clicked. 
 
-4. Object-oriented programming:
+4. Object-oriented programming (OOP): OOP is a computer programming model based on objects. OOP are closely binded or linked to their functions and allows no modication from outside functions. 
 
-5. Ruby:
+5. Ruby: Ruby is a backend language that uses object-oriented programming. Known for its simplicity, Ruby is general-purpose, dynamic, interpreted, and works on the server side of scriping language.
